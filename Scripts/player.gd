5fc9da302966c08a4acb311b2fc2c72e322e8951
VenturiVen <gingeroshea443@gmail.com>
2025-02-13 10:10:51 +0000

    Creating Folders, Scenes, Scripts

---
+++ Scripts/player.gd
@@ -0,0 +1,19 @@
+# This is the script where the player's code is handled
+
+# To leave a comment, prefix a line with a '#'
+
+# This means we are getting the attributes of a Node2D 
+# (Our 'Player' node is a Node2D)
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+	# delta is used for making movement and time-based operations
+	# consistent across different frame rates. (https://www.dragonflydb.io/faq/godot-when-to-use-delta)
+func _process(delta: float) -> void:
+	pass
